013c62d7c613b5d9e22844d62c15e0a5d7811b0d
u-Alice <umugwanezaalice22@gmail.com>
2023-02-24 11:14:33 +0200

    completed delete middle

--- linkedList/deleteMiddle.cpp
+++ linkedList/deleteMiddle.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 class Node
 {
-    public:
+public:
     int data;
     Node *next;
     Node(int d)
@@ -13,17 +13,50 @@ class Node
     }
 };
 
-Node* deleteMiddle(Node *head,int data){
-    while (head->next != NULL){
+Node *deleteMiddle(Node *head, int data)
+{
+    if (head == NULL)
+    {
+        return NULL;
+    }
+    if (head->data == data)
+    {
+        Node *temp = head;
+        head = head->next;
+        delete temp;
+        return head;
+    }
+    while (head->next != NULL)
+    {
         Node *temp = head->next;
-        if(head->next->data == data){
+        if (head->next->data == data)
+        {
             head->next = head->next->next;
-            delete(temp);
+            delete (temp);
         }
+        head = head->next;
     }
 }
-
-int main(){
-
+void display(Node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << endl;
+        head = head->next;
+    }
+}
+int main()
+{
+    Node *n1 = new Node(5);
+    Node *n2 = new Node(6);
+    Node *n3 = new Node(7);
+    Node *n4 = new Node(8);
+    Node *n5 = new Node(9);
+    n1->next = n2;
+    n2->next = n3;
+    n3->next = n4;
+    n4->next = n5;
+    n1 = deleteMiddle(n1, 5);
+    display(n1);
     return 0;
 }
