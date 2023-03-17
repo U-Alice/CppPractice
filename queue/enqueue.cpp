7b12bb54559ba09f91ffb4aa198184109a92ec8b
u-Alice <umugwanezaalice22@gmail.com>
2023-03-17 17:12:51 +0200

    enqueue and dequeue

--- queue/enqueue.cpp
+++ queue/enqueue.cpp
@@ -9,23 +9,28 @@ void enqueue(int array[], int n, int element){
         cout << "The queue is full";
         return;
     }else{
+        front = 0;
         rear = rear + 1;
         array[rear] = element;
     }
     return;
 }
-void dequeue(int array[], int n, int element){
-    if (front = -1){
-        
+void dequeue(int array[]){
+    if (front == -1){
+        cout << "The queue is empty!!";
+    }else{
+        front = front + 1;
     }
 }
 void display(int array[]){
-    for(int i = 0; i < n; i++){
+    for(int i = front; i < n; i++){
         cout <<  array[i] << "\t";
     }
 }
 int main(){
     enqueue(array, 5, 1);
+    enqueue(array, 5, 4);
     display(array);
-
+    dequeue(array);
+    display(array);
 }
